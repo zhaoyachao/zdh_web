0e6bf4b8f1ed6955c700352c6efc1942562be77a
zhaoyachao <1209687056@qq.com>
2023-05-13 14:04:33 +0800

    v5.1.1优化登录页面,增加sql脚本

--- release/bin/start.bat
+++ release/bin/start.bat
@@ -1,5 +1,4 @@
 set bin_path = %~dp0
-echo %bin_path%
 cd %bin_path%../
 
 java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xss512M -jar zdh_web.jar
--- release/bin/start.sh
+++ release/bin/start.sh
@@ -1,8 +1,6 @@
 set ff=unix
 bin_path=`dirname "$0"`
-echo $bin_path
 cd "$bin_path/.."
 pt=`pwd`
-echo $pt
-echo "java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xss512M -jar zdh_web.jar"
+#echo "java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xss512M -jar zdh_web.jar"
 nohup java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xss512M -jar zdh_web.jar >> web.log  &
