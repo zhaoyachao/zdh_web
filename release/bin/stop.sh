c6ab63aff28a53c0bf12e1c1ef69ee09706626d8
zhaoyachao <1209687056@qq.com>
2023-07-08 19:01:40 +0800

    fix: 修复脚本

--- release/bin/stop.sh
+++ release/bin/stop.sh
@@ -3,4 +3,4 @@ bin_path=`dirname "$0"`
 cd "$bin_path/.."
 pt=`pwd`
 APP_NAME=${pt}"/zdh_web.jar"
-kill -15 `ps -ef |grep "APP_NAME" |grep java|awk -F " " '{print $2}'`
+kill -15 `ps -ef |grep "${APP_NAME}" |grep java|awk -F " " '{print $2}'`
