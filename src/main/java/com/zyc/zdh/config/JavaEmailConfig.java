37efac43830e3042ad6d5e48caf39286a1d42e6e
zhaoyachao <1209687056@qq.com>
2024-11-24 18:39:00 +0800

    v5.5.5 优化邮件配置,删除自定义邮件配置

--- src/main/java/com/zyc/zdh/config/JavaEmailConfig.java
+++ src/main/java/com/zyc/zdh/config/JavaEmailConfig.java
@@ -1,14 +1,7 @@
 package com.zyc.zdh.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import java.util.Properties;
 
 
 @Configuration
@@ -48,24 +41,26 @@ public class JavaEmailConfig {
         this.password = password;
     }
 
-    @Bean(name = "javaMailSender")
-    public JavaMailSender getSender(){
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setUsername(username);
-        javaMailSender.setHost(host);
-        javaMailSender.setPort(Integer.parseInt(port));//①
-        javaMailSender.setDefaultEncoding("UTF-8");
-        Properties props = new Properties();//②
-        props.setProperty("mail.smtp.host", host);
-        props.setProperty("mail.smtp.auth", "true");
-        javax.mail.Session session = javax.mail.Session.getDefaultInstance(props,new Authenticator(){
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication(){
-                return new PasswordAuthentication(username,password);
-            }
-        });
-        javaMailSender.setSession(session);//③
-        return javaMailSender;
-    }
+//    @Bean(name = "javaMailSender")
+//    public JavaMailSender getSender(){
+//        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+//        javaMailSender.setUsername(username);
+//        javaMailSender.setHost(host);
+//        javaMailSender.setPort(Integer.parseInt(port));//①
+//        javaMailSender.setDefaultEncoding("UTF-8");
+//        Properties props = new Properties();//②
+//        props.setProperty("mail.smtp.host", host);
+//        props.setProperty("mail.smtp.auth", "true");
+//
+//
+//        javax.mail.Session session = javax.mail.Session.getDefaultInstance(props,new Authenticator(){
+//            @Override
+//            protected PasswordAuthentication getPasswordAuthentication(){
+//                return new PasswordAuthentication(username,password);
+//            }
+//        });
+//        javaMailSender.setSession(session);//③
+//        return javaMailSender;
+//    }
 
 }
